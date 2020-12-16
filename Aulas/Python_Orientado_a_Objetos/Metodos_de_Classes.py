4d27f9f920ac62055fd901a3cbcd4b726bba37d4
edersonhs <edersonhs3217@gmail.com>
2020-12-16 20:23:31 -0300

    lesson 96

--- Aulas/Python_Orientado_a_Objetos/Metodos_de_Classes.py
+++ Aulas/Python_Orientado_a_Objetos/Metodos_de_Classes.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Pessoa:
     ano_atual = 2019   # Atributo de classe  /  Esta disponivel pela classe
 
@@ -19,6 +21,13 @@ class Pessoa:
 
         return cls(nome, idade)   # Retorna o objeto da da propria classe baseado nos parametros passados. É semelhante ao que foi feito no escopo global. EX: p1 = Pessoa('Luiz', 32)
         # cls(nome, idade) esta executando a classe Pessoa
+    
+    @staticmethod
+    def gera_id():   # Pode receber parametros, mas não recebe a classe ou instancia
+        rand = randint(1, 100)
+        return rand
+
+
 
 p1 = Pessoa('Luiz', 32)
 print(p1.idade)
@@ -26,8 +35,21 @@ p1.get_ano_nascimento()
 
 print()
 
-
 p2 = Pessoa.por_ano_nascimento('João', 1987)   # Retorna Pessoa(nome, idade)
 print(p2)   # <__main__.Pessoa object at 0x000001E906571F10>   / __main__ = Nme do modulo   /   Pessoa = Nome da classe   /   object at 0x000001E906571F10 = Local do objeto na memoria
 print(p2.nome, p2.idade)   # João 32
 p2.get_ano_nascimento()   # 1987
+
+print()
+
+print(Pessoa.gera_id())
+print(p1.gera_id())   # Caso seja necessario, é possivel chamar o metodo estatico pela instancia também
+
+"""
+Método de instancia: precisam de uma instancia para funcionar
+
+Método de classe: não precisa da instancia mas sim a classe em sí
+
+Metodo Estatico: não precisa nem da instancia nem da classe, tanto que a classe e a instancia não podem ser referenciados nele,
+* É como uma função normal que por questões de organização ficou na classe
+"""
