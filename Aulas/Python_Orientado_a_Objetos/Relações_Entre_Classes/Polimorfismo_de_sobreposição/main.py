2cc1a120fe31d40190ff15ef587cf4cd35abfd69
edersonhs <edersonhs3217@gmail.com>
2020-12-29 15:10:35 -0300

    lesson 106 update

--- Aulas/Python_Orientado_a_Objetos/Relações_Entre_Classes/Polimorfismo_de_sobreposição/main.py
+++ Aulas/Python_Orientado_a_Objetos/Relações_Entre_Classes/Polimorfismo_de_sobreposição/main.py
@@ -1,6 +1,7 @@
 """
 Polimorfismo é o principio que permite que classes derivadas de uma mesma
-superclasse tenham métodos iguais (de mesma assinatura) mas comportamentos diferentes.
+superclasse tenham métodos iguais (de mesma assinatura) mas comportamentos
+diferentes.
 
 Mesma assinatura = Mesma quantidade e tipo de parâmetros
 """
