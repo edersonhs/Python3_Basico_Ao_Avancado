7b7be74ca8988cad4d0d39d05ab47371e531e38f
edersonhs <edersonhs3217@gmail.com>
2021-01-28 22:21:50 -0300

    lesson 124

---
+++ Aulas/Módulos_Python/Datetime-Trabalhando_com_Data_e_Hora/Example1.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+"""
+Timedelta = Duração/Intervalo de tempo
+"""
+
+# Instanciando uma data
+data = datetime(2021, 1, 10, 10, 53, 20)  # Hora/minuto/segundo são opcionais
+"""
+datetime(Ano, mês, dia, hora, minuto e segundo)
+"""
+print(data)   # 2021-01-10 10:53:20 padrão americano
+
+# Formatando a data manualmente com o metodo strftime da instancia
+print(data.strftime('%d/%m/%Y %H:%M:%S'))   # 10/01/2021 10:53:20
+"""
+As diretivas (%d, %m, %Y, %H, %M, %S...) podem ser encontradas na documentação
+oficial do modulo.
+"""
+
+# Normalmente os dados de data e hora não são recebidos divididos por partes,
+# por isto usamos a função strptime() para tornar a string em um objeto de data
+data1 = datetime.strptime('20/04/2019', '%d/%m/%Y')
+"""
+ # strptime('', '') recebe uma string e um formato.
+"""
+print(data1)   # 2019-04-20 00:00:00 Segundos zerados por não serem definidos
+
+"""
+Timestamp()   # Contagem de segundos desde 01/01/1970 até a data definida na
+instancia.
+
+Com um timestamp, podemos fazer a convesão do mesmo para a data
+atual.
+"""
+# print(data1.timestamp())   # 1555729200.0
+
+# Convertendo o timestamp para data
+data3 = datetime.fromtimestamp(1555729200.0)
+print(data3)
---
+++ Aulas/Módulos_Python/Datetime-Trabalhando_com_Data_e_Hora/example2.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+
+data = datetime.strptime('20/04/1987 20:00:00', '%d/%m/%Y %H:%M:%S')
+# Somando + 5 dias e 59 segundos na data
+data += timedelta(days=5, seconds=59)
+
+print(data.strftime('%d/%m/%Y %H:%M:%S'))   # 25/04/1987 20:00:00
+
+print()
+
+"""
+Com timedelta é possivel somar, subtrair, comparar datas e muito mais
+"""
+d1 = datetime.strptime('20/04/1987 20:00:00', '%d/%m/%Y %H:%M:%S')
+d2 = datetime.strptime('25/04/1987 22:33:00', '%d/%m/%Y %H:%M:%S')
+diferenca = d2 - d1   # retorna um "timedelta"
+print(f'{diferenca} de diferença.')   # 5 days, 2:33:00
+print(diferenca.total_seconds())   # somente o total de segundos de diferença
+# print(diferenca.days)   # Apenas os dias de diferença entre as duas datas
+
+# print(d1.time())   # Retorna apenas a hora de d1
+
+# print(d2 > d1)   # True - a data2 é maior que a data1
