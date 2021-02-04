c052e8b68425bb9aa7ce2cf1e2572569167062ac
edersonhs <edersonhs3217@gmail.com>
2021-02-03 22:29:13 -0300

    lesson127

---
+++ Aulas/Módulos_Python/OS+SHUTIL-Mover_Copiar_e_Apagar_Arquivos/Example.py
@@ -0,0 +1,39 @@
+"""
+# Usa o r no inicio para poder usar a contrabarra
+caminho_windows = r'C:\programas\exemplo'
+"""
+import os
+import shutil   # MOVER E COPIAR ARQUIVOS
+
+caminho_original = r'D:\Documents'
+caminho_novo = r'D:\Documents\Exemplo'
+
+try:
+    os.mkdir(caminho_novo)   # Cria a pasta
+except FileExistsError:
+    print(f'Pasta {caminho_novo} já existe.')
+
+for root, dir, files in os.walk(caminho_original):
+    """
+    root: Caminho do diretorio original até o arquivo
+    dir: lista com os diretorios dentro do diretorio original
+    files: lista com os arquivos contidos do diretorio original em diante
+    """
+    for file in files:
+        old_file_path = os.path.join(root, file)  # caminho antigo do arquivo
+        new_file_path = os.path.join(
+            caminho_novo, file)   # Caminho novo do arquivo
+
+        # Copiar o arquivo de uma pasta para outra pasta
+        shutil.copy(old_file_path, new_file_path)
+        print(f'Arquivo {file} copiado com sucesso!')
+
+        # Mover o arquivo do caminho antigo para o novo caminho
+        shutil.move(old_file_path, new_file_path)
+        print(f'Arquivo {file} movido com sucesso!')
+
+        """
+        shutil.move também serve para renomear arquivos, desde que o caminho
+        original esteja correto pode-se inserir o novo nome desejado no
+        caminho de destino
+        """
