fb9679d07073dcdfe0ab5a1286b1ad30ac3c23bf
edersonhs <edersonhs3217@gmail.com>
2021-03-21 17:20:03 -0300

    Push Instalando_QT_Designer_no_Windows.py

---
+++ Aulas/PyQT5-Interface_grafica_no_python-GUI_para_desktop/Instalando_QT_Designer_no_Windows.py
@@ -0,0 +1,26 @@
+""" QT Designer: Programa com interface grafica para fazer interfaces gráficas no Python"""
+
+""" Instalar e remover no Windows """
+
+# Instalar via PIP
+# pip install pyqt5-tools
+
+# Remover do Windows
+# pip uninstall pyqt5-tools
+
+# Após Instalar, oexecutavel do QTdesigner fica no diretorio raiz do módulo nos dados do Python
+# Para acessar:
+
+# Abre o executar e usa o comando: (O caminho pode ser diferente)
+# %LOCALAPPDATA%\Programs\Python\Python39\Lib\site-packages\pyqt5_tools
+
+# Nome do QTDesigner: desiginer.exe
+
+
+""" Novas versões do PyQT5 não estão trasendo o QTDesigner no pacote"""
+
+# Se isso ocorrer com você, baixe o QT Designer no seguinte site:
+
+# https://build-system.fman.io/qt-designer-download (recomendável)
+
+# https://drive.google.com/file/d/1_ahJyoQRhldrrLA_CiwQ1jk6xXxTYzSj/view?usp=sharing (meu gdrive)
