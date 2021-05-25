ad91e94c92ee169eb39ce552c93b174f036424a8
edersonhs <edersonhs3217@gmail.com>
2021-05-24 21:15:03 -0300

    Push agenda.py

---
+++ Exercicios/Agenda/app.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+
+class AgendaDB:
+    def __init__(self, arquivo):
+        self.conn = sqlite3.connect(arquivo)
+        self.cursor = self.conn.cursor()
+
+    def inserir(self, nome, telefone):
+        consulta = 'INSERT OR IGNORE INTO agenda (nome, telefone) VALUES (?, ?)'
+        self.cursor.execute(consulta, (nome, telefone))
+        self.conn.commit()
+
+    def editar(self, nome, telefone, id):
+        consulta = 'UPDATE OR IGNORE agenda SET nome=?, telefone=? WHERE id=?'
+        self.cursor.execute(consulta, (nome, telefone, id))
+        self.conn.commit()
+
+    def excluir(self, id):
+        consulta = 'DELETE FROM agenda WHERE id=?'
+        self.cursor.execute(consulta, (id,))
+        self.conn.commit()
+
+    def listar(self):
+        self.cursor.execute('SELECT * FROM agenda')
+
+        for linha in self.cursor.fetchall():
+            print(linha)
+
+    def buscar(self, valor):
+        consulta = 'SELECT * FROM agenda WHERE nome LIKE ?'
+        self.cursor.execute(consulta, (f'%{valor}%',))
+
+        for linha in self.cursor.fetchall():
+            print(linha)
+
+    def fechar(self):
+        self.cursor.close()
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    agenda = AgendaDB('exercicios/agenda/agenda.db')
+
+    agenda.inserir('Luiz Otavío', '102321')
+    agenda.inserir('Luiz Felipe', '102322')
+    agenda.inserir('Ronaldo Luiz', '102323')
+    agenda.inserir('R. Luiza', '102324')
+    agenda.inserir('Maria Luiza de Souza', '102325')
+    agenda.buscar('Luiz')
